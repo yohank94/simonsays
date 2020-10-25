4509b328f98bf410508343a23de7c55c0c5ac47e
Yohanes Kurnia <yohanes.kurnia@hotmail.com>
2020-10-25 02:16:21 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      queue: [],
+      answerQueue: [],
+      counter: 0
+    };
+
+    this.toggleStart = this.toggleStart.bind(this);
+    this.handleButton = this.handleButton.bind(this);
+  }
+
+  toggleStart() {
+    this.setState(prevState => ({
+      playing: !prevState.playing
+    }), () => {
+        console.log("Done toggleStart");
+        if (this.state.playing) this.generateNums();
+        if (!this.state.playing) this.deleteNums();
+    });
+  }
+
+  generateNums() {
+    const num = Math.floor(Math.random() * 4) + 1;
+    this.setState(state => ({
+      queue: [...state.queue, num]
+    }), () => {
+      console.log(this.state.queue);
+      //this.deletePreviousAnswer();
+    });
+  }
+
+  deleteNums() {
+    this.setState(state => ({ playing: !state.playing, queue: [], answerQueue: [], counter: 0 }));
+  }
+
+  deletePreviousAnswer() {
+    this.setState(state => ({
+      answerQueue: [],
+      counter: 0
+    }),() => {
+      console.log("DPA Cntr: " + this.state.counter)
+      this.generateNums()
+    });
+  }
+
+  handleButton(e) {
+    //e.preventDefault();
+    const num = parseInt(e.target.innerHTML);
+    if (this.state.playing) {
+      this.setState(state => ({
+        answerQueue: [...state.answerQueue, num],
+        counter: state.counter + 1
+      }), () => {
+        console.log(JSON.stringify(this.state.answerQueue));
+        let cntr = this.state.counter;
+        console.log("Cntr: " + cntr);
+        console.log("Queue Length: " + this.state.queue.length);
+        console.log(this.state.queue[cntr - 1] !== this.state.answerQueue[cntr - 1]);
+        if (this.state.queue[cntr - 1] !== this.state.answerQueue[cntr - 1]) {
+          this.deleteNums();
+        } else {
+          if (cntr === this.state.queue.length) {
+            this.deletePreviousAnswer();
+          } else {
+          }
+        }
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Playing?: {this.state.playing ? "true" : "false" }</p>
+        <p>Queue: {this.state.queue}</p>
+        <p>Answer Queue: {this.state.answerQueue}</p>
+        <p>Counter: {this.state.counter}</p>
+        <button onClick={this.handleButton}>1</button>
+        <button onClick={this.handleButton}>2</button>
+        <button onClick={this.handleButton}>3</button>
+        <button onClick={this.handleButton}>4</button>
+        <button id='start-btn' onClick={this.toggleStart}>Start</button>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/App.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './App';
+
+test('renders Hello World', () => {
+  const component = renderer.create(
+    <App />
+  );
+  
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+})
